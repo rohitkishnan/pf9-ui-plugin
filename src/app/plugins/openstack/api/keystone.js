5675293e4cfb03c87c16fe1f240e6aae591de8cc
Brennan Cheung <git@brennancheung.com>
2018-02-08 18:13:03 -0800

    Port over util and keystone API

---
+++ src/app/plugins/openstack/api/keystone.js
@@ -0,0 +1,229 @@
+import {
+  authOpenstackHttp,
+  bareJson,
+  makeApi,
+  memoize,
+} from '../util'
+
+const bareHttp = makeApi(bareJson, '/keystone/v3')
+const v3 = makeApi(authOpenstackHttp, '/keystone/v3') // baseUrl
+const admin = makeApi(authOpenstackHttp, '/keystone_admin/v2.0') // adminUrl
+
+const constructUrlQuery = (...parts) => parts.join('&')
+const constructTokenBody = method => (tenantId, unscopedToken) => ({
+  auth: {
+    identity: {
+      methods: [method],
+      [method]: { id: unscopedToken },
+    },
+    scope: { project: { id: tenantId } }
+  }
+})
+
+let endpointsPromise
+
+export const getUsers = () => v3.getReq('/users')
+export const getUser = userId => v3.getReq(`/users/${userId}`)
+export const getTenants = () => admin.getReq('/PF9-KSADM/all_tenants_all_users')
+export const getScopedProjects = () => v3.getReq('/auth/projects')
+
+export function createTenant (project, success, error) {
+  const body = {
+    project: {
+      enabled: project.enabled,
+      name: project.name,
+      description: project.description,
+      domain_id: 'default',
+      is_domain: false
+    }
+  }
+  return v3.postReq('/projects', body).then(success, error)
+}
+
+export function updateTenant (project, success, error) {
+  const body = {
+    project: {
+      enabled: project.enabled,
+      name: project.name,
+      description: project.description,
+      domain_id: 'default',
+      is_domain: false
+    }
+  }
+  return v3.patchReq(`/projects/${project.id}`, body).then(success, error)
+}
+
+export const deleteTenant = (id, success, error) => v3.deleteReq(`/projects/${id}`).then(success, error)
+export const getRoles = (success, error) => v3.getReq('/roles').then(success, error)
+export const getUsersFor = tenantId => admin.getReq(`/tenants/${tenantId}/users`)
+export const removeUser = (userId, success, error) => v3.deleteReq(`/users/${userId}`).then(success, error)
+
+export function getUserRoles (tenantId, userId, success, error) {
+  const url = constructUrlQuery(
+    `/role_assignments?user.id=${userId}`,
+    `scope.project.id=${tenantId}`,
+    `effective&include_names`
+  )
+  return v3.getReq(url).then(success, error)
+}
+
+export function addUser (user, success, error) {
+  const body = {
+    user: {
+      name: user.name,
+      email: user.name,
+      password: user.password,
+      default_project_id: user.tenantId
+    }
+  }
+  body.user.displayname = user.displayName || null
+  return v3.postReq('/users', body).then(success, error)
+}
+
+export function updateUser (user, success, error) {
+  const body = {
+    user: {
+      name: user.name,
+      email: user.name
+    }
+  }
+  body.user.displayname = user.displayname ? user.displayname : null
+  if (user.password) {
+    body.user.password = user.password
+  }
+  return v3.patchReq(`/users/${user.id}`, body).then(success, error)
+}
+
+export function addUserRole (tenantId, userId, roleId, success, error) {
+  const url = `/projects/${tenantId}/users/${userId}/roles/${roleId}`
+  return v3.putReq(url).then(success, error)
+}
+
+export function removeUserRole (tenantId, userId, roleId, success, error) {
+  const url = `/projects/${tenantId}/users/${userId}/roles/${roleId}`
+  return v3.deleteReq(url).then(success, error)
+}
+
+export function updateGlanceEndpoint (endpointId, endpointUrl) {
+  const body = {
+    endpoint: { url: `http://${endpointUrl}:9292` }
+  }
+  return v3.patchReq(`/endpoints/${endpointId}`, body)
+}
+
+export function addGlanceEndpoint (regionId, endpointUrl, serviceId) {
+  const body = {
+    endpoint: {
+      'interface': 'public',
+      region_id: regionId,
+      service_id: serviceId,
+      url: `http://${endpointUrl}:9292`
+    }
+  }
+  return v3.postReq('/endpoints', body)
+}
+
+export const deleteGlanceEndpoint = endpointId => v3.deleteReq(`/endpoints/${endpointId}`)
+
+export const getGroups = () => v3.getReq('/groups')
+export const createGroup = group => v3.post('/groups', { group })
+export const updateGroup = (group, groupId) => v3.patchReq(`/groups/${groupId}`, { group })
+
+export const getGroupRoles = (tenantId, groupId) => v3.getReq(`/projects/${tenantId}/groups/${groupId}/roles`)
+
+export const addGroupRole = (tenantId, groupId, roleId) =>
+  v3.putReq(`/projects/${tenantId}/groups/${groupId}/roles/${roleId}`)
+
+export const removeGroupRole = (tenantId, groupId, roleId) =>
+  v3.deleteReq(`/projects/${tenantId}/groups/${groupId}/roles/${roleId}`)
+
+export const deleteGroup = groupId => v3.deleteReq(`/groups/${groupId}`)
+
+export function getUnscopedToken (username, password) {
+  const body = {
+    auth: {
+      identity: {
+        methods: ['password'],
+        password: {
+          user: {
+            name: username,
+            domain: { id: 'default' },
+            password: password
+          }
+        }
+      }
+    }
+  }
+  return bareHttp.postReq('/auth/tokens?nocatalog', body)
+}
+
+export const getUnscopedTokenSso = () => v3.getReq('/OS-FEDERATION/identity_providers/IDP1/protocols/saml2/auth')
+
+export function getScopedToken (tenantId, unscopedToken) {
+  const body = constructTokenBody('token')(tenantId, unscopedToken)
+  return bareHttp.postReq('/auth/tokens?nocatalog', body)
+}
+
+export function getScopedTokenWithCatalog (tenantId, unscopedToken, success, error) {
+  const body = constructTokenBody('token')(tenantId, unscopedToken)
+  return bareHttp.postReq('/auth/tokens', body).then(success, error)
+}
+
+export function getScopedTokenSso (tenantId, unscopedToken, success, error) {
+  const body = constructTokenBody('saml2')(tenantId, unscopedToken)
+  return bareHttp.postReq('/auth/tokens?nocatalog', body).then(success, error)
+}
+
+export function getScopedTokenWithCatalogSso (tenantId, unscopedToken, success, error) {
+  const body = constructTokenBody('saml2')(tenantId, unscopedToken)
+  return bareHttp.postReq('/auth/tokens', body).then(success, error)
+}
+
+export const getServiceCatalog = () => v3.getReq('/auth/catalog')
+
+export const getServices = () => memoize('keystone-services', () => v3.getReq('/services'))
+export const getRegions = () => memoize('keystone-regions', () => v3.getReq('/regions'))
+
+export function getEndpoints (refresh) {
+  if (endpointsPromise && !refresh) {
+    return endpointsPromise
+  }
+  endpointsPromise = v3.getReq('/endpoints')
+  return endpointsPromise
+}
+
+export const isMfaEnabled = (userId, success, error) => admin.getReq(`/PF9-KSADM/users/${userId}/mfa`).then(success, error)
+
+export function enableMfa (userId, secret, success, error) {
+  const body = {
+    mfa: { enabled: true, secret: secret }
+  }
+  return admin.putReq(`/PF9-KSADM/users/${userId}/mfa`, body).then(success, error)
+}
+
+export function disableMfa (userId, success, error) {
+  const body = {
+    mfa: { enabled: false }
+  }
+  return admin.putReq(`/PF9-KSADM/users/${userId}/mfa`, body).then(success, error)
+}
+
+export const getIdentityProviders = () => v3.getReq('/OS-FEDERATION/identity_providers')
+export const getGroupMappings = () => v3.getReq('/OS-FEDERATION/mappings')
+export const getMappingById = mappingId => v3.getReq(`/OS-FEDERATION/mappings/${mappingId}`)
+export const createGroupMapping = (mappingId, mapping) => v3.putReq(`/OS-FEDERATION/mappings/${mappingId}`, { mapping })
+export const editGroupMapping = (mappingId, mapping) => v3.patchReq(`/OS-FEDERATION/mappings/${mappingId}`, { mapping })
+export const deleteGroupMapping = mappingId => v3.deleteReq(`/OS-FEDERATION/mappings/${mappingId}`)
+export const getIdpProtocols = identityProviderId => v3.getReq(`/OS-FEDERATION/identity_providers/${identityProviderId}/protocols`)
+
+export function addIdpProtocol (identityProviderId, mappingId) {
+  const body = {
+    protocol: { mapping_id: mappingId }
+  }
+  return v3.putReq(`/OS-FEDERATION/identity_providers/${identityProviderId}/protocols/saml2`, body)
+}
+
+export const deleteIdpProtocol = identityProviderId =>
+  v3.deleteReq(`/OS-FEDERATION/identity_providers/${identityProviderId}/protocols/saml2`)
+
+export const getServiceProviders = () => v3.getReq('/OS-FEDERATION/service_providers')
