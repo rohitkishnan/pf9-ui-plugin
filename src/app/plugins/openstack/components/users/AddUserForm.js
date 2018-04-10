e1a2d6cd297c92d915585cf2af9d83fdb0b1171e
Brennan Cheung <git@brennancheung.com>
2018-04-09 17:53:29 -0700

    Set up Storybook further and add User examples

---
+++ src/app/plugins/openstack/components/users/AddUserForm.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import NoAutofillHack from '../common/NoAutofillHack'
+import TextField from 'material-ui/TextField'
+import Button from 'material-ui/Button'
+
+class AddUserForm extends React.Component {
+  state = {
+    name: '',
+    displayname: '',
+    password: '',
+  }
+
+  setField = field => event => this.setState({ [field]: event.target.value })
+
+  fields = [
+    { id: 'name', label: 'Username' },
+    { id: 'displayname', label: 'Display Name' },
+    { id: 'password', label: 'Password', type: 'password' },
+  ]
+
+  renderField = ({ id, label, type = 'text' }) => {
+    if (type === 'text' || type === 'password') {
+      return (
+        <div key={id}>
+          <TextField id={id} type={type} label={label} value={this.state[id]} onChange={this.setField(id)} />
+        </div>
+      )
+    }
+  }
+
+  handleSubmit = () => {
+    this.props.onSubmit(this.state)
+  }
+
+  render () {
+    return (
+      <form noValidate>
+        <NoAutofillHack />
+        {this.fields.map(this.renderField)}
+        <div>
+          <Button variant="raised" onClick={this.handleSubmit}>Submit</Button>
+        </div>
+      </form>
+    )
+  }
+}
+
+AddUserForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired
+}
+
+export default AddUserForm
--- stories/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
-
-storiesOf('Storybook', module)
-  .add('it is working', () => (
-    <div>Storybook is working</div>
-  ))
