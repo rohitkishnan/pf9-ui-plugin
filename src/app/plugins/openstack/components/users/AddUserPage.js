9c568abff4ace53f6fe13921b8883aa8903627c1
Brennan Cheung <git@brennancheung.com>
2018-04-11 14:23:00 -0700

    Add user to redux and redirect after submit

--- src/app/plugins/openstack/components/users/AddUserPage.js
+++ src/app/plugins/openstack/components/users/AddUserPage.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
 import { addUser } from '../../actions/users'
 import AddUserForm from './AddUserForm'
 
 const mapStateToProps = state => ({})
 
+@withRouter
 @connect(mapStateToProps)
 class AddUserPage extends React.Component {
   handleSubmit = user => {
+    const { dispatch, history } = this.props
     try {
-      this.props.dispatch(addUser(user))
+      dispatch(addUser(user))
+      history.push('/users')
     } catch (err) {
       console.error(err)
     }
