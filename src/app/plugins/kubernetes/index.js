1875e6a23182ac239a7b6a0a52cf93d88786540f
Brennan Cheung <git@brennancheung.com>
2018-10-09 11:12:36 -0700

    Placeholder navigation for K8s (#186)

--- src/app/plugins/kubernetes/index.js
+++ src/app/plugins/kubernetes/index.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import k8Schema from './api/schema/schema'
-
-import ClustersListContainer from './components/clusters/ClustersListContainer'
 
 class Kubernetes extends React.Component {
   render () {
@@ -13,31 +10,54 @@ class Kubernetes extends React.Component {
 
 Kubernetes.__name__ = 'kubernetes'
 
+const InfrastructurePage = () => <h1>Infrastructure Page</h1>
+const AppCatalogPage = () => <h1>App Catalog Page</h1>
+const PodsPage = () => <h1>Pods, Deployments, Services Page</h1>
+const StorageClassesPage = () => <h1>Storage Classes Page</h1>
+const NamespacesPage = () => <h1>Namespaces Page</h1>
+
 Kubernetes.registerPlugin = pluginManager => {
   pluginManager.registerRoutes(
     '/ui/kubernetes',
     [
       {
-        name: 'Clusters',
-        link: { path: '/clusters', exact: true },
-        component: ClustersListContainer
+        name: 'Infrastructure',
+        link: { path: '/infrastructure', exact: true },
+        component: InfrastructurePage
+      },
+      {
+        name: 'App Catalog',
+        link: { path: '/apps', exact: true },
+        component: AppCatalogPage
+      },
+      {
+        name: 'Pods, Deployments, Services',
+        link: { path: '/pods', exact: true },
+        component: PodsPage
+      },
+      {
+        name: 'Storage Classes',
+        link: { path: '/storage_classes', exact: true },
+        component: StorageClassesPage
+      },
+      {
+        name: 'Namespaces',
+        link: { path: '/namespaces', exact: true },
+        component: NamespacesPage
       },
     ]
   )
 
-  /*
   pluginManager.registerNavItems(
     '/ui/kubernetes',
     [
-      {
-        name: 'Clusters',
-        link: { path: '/clusters' }
-      },
+      { name: 'Infrastructure', link: { path: '/infrastructure' } },
+      { name: 'App Catalog', link: { path: '/apps' } },
+      { name: 'Pods, Deployments, Services', link: { path: '/pods' } },
+      { name: 'Storage Classes', link: { path: '/storage_classes' } },
+      { name: 'Namespaces', link: { path: '/namespaces' } },
     ]
   )
-  */
-
-  pluginManager.registerSchema(k8Schema)
 }
 
 export default Kubernetes
