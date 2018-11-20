e3d374b8bcd857669566cb149676cf96e62d8519
Xan Torres <xan.torres@gmail.com>
2018-11-20 19:13:45 +0100

    Refactor pluginManager by moving methods to registerPlugin closure, so that methods will be available from a plugin instance only

--- src/app/core/pluginManager.js
+++ src/app/core/pluginManager.js
@@ -7,72 +7,16 @@ const defaultOptions = {
   showSidebar: false,
 }
 
+const initData = () => ({
+  components: [],
+  routes: [],
+  navItems: [],
+  options: { ...defaultOptions },
+})
+
 const pluginList = {}
-const data = {}
 
 const pluginManager = {
-  clearAll (pluginId) {
-    data[pluginId] = {
-      components: [],
-      routes: [],
-      navItems: [],
-      options: { ...defaultOptions },
-    }
-  },
-
-  registerPlugin (pluginId, name, basePath) {
-    pluginList[pluginId] = {
-      name,
-      basePath,
-      registerComponent: (...args) => this.registerComponent(pluginId, ...args),
-      registerRoutes: (...args) => this.registerRoutes(pluginId, ...args),
-      registerNavItems: (...args) => this.registerNavItems(pluginId, ...args),
-      getComponents: () => this.getComponents(pluginId),
-      getRoutes: () => this.getRoutes(pluginId),
-      getNavItems: () => this.getNavItems(pluginId),
-      getOptions: () => this.getOptions(pluginId),
-      getOption: (...args) => this.getOption(pluginId, ...args),
-      setOption: (...args) => this.setOption(pluginId, ...args),
-      getDefaultRoute: () => this.getDefaultRoute(pluginId),
-      clearAll: (...args) => this.clearAll(pluginId, ...args),
-    }
-    data[pluginId] = {
-      basePath,
-      components: [],
-      routes: [],
-      navItems: [],
-      options: { ...defaultOptions },
-    }
-    return pluginList[pluginId]
-  },
-
-  registerComponent (pluginId, component) {
-    data[pluginId].components.push(component)
-  },
-
-  registerRoutes (pluginId, components=[]) {
-    const prefixLink = link => ({
-      ...link,
-      path: pathJoin(data[pluginId].basePath, link.path)
-    })
-
-    components
-      .map(c => ({ ...c, link: prefixLink(c.link) }))
-      .forEach(component => data[pluginId].routes.push(component))
-  },
-
-  registerNavItems (pluginId, items=[]) {
-    const prefixLink = link => ({
-      ...link,
-      path: pathJoin(data[pluginId].basePath, link.path)
-    })
-
-    items
-      .map(x => ({ ...x, link: prefixLink(x.link) }))
-      .forEach(item => {
-        data[pluginId].navItems.push(item)
-      })
-  },
 
   getPlugins () {
     return pluginList
@@ -82,33 +26,77 @@ const pluginManager = {
     return pluginList[pluginId]
   },
 
-  getComponents (pluginId) {
-    return data[pluginId].components
-  },
+  registerPlugin: (pluginId, name, basePath) => {
+    let data = initData()
 
-  getRoutes (pluginId) {
-    return data[pluginId].routes
-  },
+    pluginList[pluginId] = {
+      name,
 
-  getNavItems (pluginId) {
-    return data[pluginId].navItems
-  },
+      basePath,
 
-  getOptions (pluginId) {
-    return data[pluginId].options
-  },
+      clearAll: () => {
+        data = initData()
+      },
 
-  getOption (pluginId, key) {
-    return data[pluginId].options[key]
-  },
+      registerComponent: (component) => {
+        data.components.push(component)
+      },
 
-  setOption (pluginId, key, value) {
-    data[pluginId].options[key] = value
-  },
+      registerRoutes: (components=[]) => {
+        const prefixLink = link => ({
+          ...link,
+          path: pathJoin(basePath, link.path)
+        })
 
-  getDefaultRoute (pluginId) {
-    return path(['link', 'path'],
-      data[pluginId].routes.find(r => r.link && r.link.default))
+        components
+          .map(c => ({ ...c, link: prefixLink(c.link) }))
+          .forEach(component => data.routes.push(component))
+      },
+
+      registerNavItems: (items=[]) => {
+        const prefixLink = link => ({
+          ...link,
+          path: pathJoin(basePath, link.path)
+        })
+
+        items
+          .map(x => ({ ...x, link: prefixLink(x.link) }))
+          .forEach(item => {
+            data.navItems.push(item)
+          })
+      },
+
+      getComponents: () => {
+        return data.components
+      },
+
+      getRoutes: () => {
+        return data.routes
+      },
+
+      getNavItems: () => {
+        return data.navItems
+      },
+
+      getOptions: () => {
+        return data.options
+      },
+
+      getOption: (key) => {
+        return data.options[key]
+      },
+
+      setOption: (key, value) => {
+        data.options[key] = value
+      },
+
+      getDefaultRoute: () => {
+        return path(['link', 'path'],
+          data.routes.find(r => r.link && r.link.default))
+      }
+    }
+
+    return pluginList[pluginId]
   }
 }
 
