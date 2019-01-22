a323b71db1b5866d8eace364e76f94d6b2e68362
Xan Torres <xan.torres@gmail.com>
2019-01-22 19:59:31 +0100

    Using base config pack in storybook webpack.config

--- src/stories/webpack.config.js
+++ src/stories/webpack.config.js
@@ -2,31 +2,18 @@
 // main config.  It does provide a mechanism to extend it.
 // Aliases have been added so we don't have to figure out the relative paths.
 
-const path = require('path')
-const includePath = path.resolve(__dirname, '../app')
+const R = require('ramda')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const baseConfig = require('../../webpack.config.js')
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        include: includePath,
-        exclude: /node_modules/,
-        use: ['style-loader', 'css-loader']
-      },
-    ],
-  },
-  resolve: {
-    alias: {
-      // IDE's seem to solve paths according to the order in which they are defined
-      // so we must put first the more specific aliases
-      developer: path.resolve(__dirname, '../app/plugins/developer'),
-      k8s: path.resolve(__dirname, '../app/plugins/kubernetes'),
-      openstack: path.resolve(__dirname, '../app/plugins/openstack'),
-      core: path.resolve(__dirname, '../app/core'),
-      utils: path.resolve(__dirname, '../app/utils'),
-      app: path.resolve(__dirname, '../app'),
-    },
-    extensions: ['.webpack.js', '.web.js', '.js', '.json', '.css'],
-  }
-}
+module.exports = merge(
+  R.pick(['module', 'resolve'], baseConfig),
+  {
+    devtool: 'cheap-module-eval-source-map',
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': 'development'
+      })
+    ]
+  })
