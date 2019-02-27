c99741ac695910a79c235d558b6288f9709205b2
Xan Torres <xan.torres@gmail.com>
2019-02-27 21:37:57 +0100

    Move loadRepositories into DataLoader and code cleanup

--- src/app/plugins/kubernetes/components/apps/RepositoriesListPage.js
+++ src/app/plugins/kubernetes/components/apps/RepositoriesListPage.js
@@ -1,35 +1,11 @@
-import createCRUDComponents from 'core/helpers/createCRUDComponents'
 import React from 'react'
+import createCRUDComponents from 'core/helpers/createCRUDComponents'
 import { loadRepositories, deleteRepository } from 'k8s/components/apps/actions'
 import { loadInfrastructure } from 'k8s/components/infrastructure/actions'
-import { withAppContext } from 'core/AppContext'
-import Loader from 'core/components/Loader'
-import { compose } from 'ramda'
-import requiresAuthentication from 'openstack/util/requiresAuthentication'
 import { withDataLoader } from 'core/DataLoader'
 
-const ListPage = ({ ListContainer }) => {
-  class ListPage extends React.Component {
-    async componentDidMount () {
-      const { context, setContext } = this.props
-
-      await loadRepositories({
-        context,
-        setContext,
-      })
-    }
-
-    render () {
-      const { repositories } = this.props.context
-      if (!repositories) { return <Loader /> }
-      return (
-        <ListContainer data={repositories} />
-      )
-    }
-  }
-
-  return withAppContext(ListPage)
-}
+const ListPage = ({ ListContainer }) =>
+  ({ data: { repositories } }) => <ListContainer data={repositories} />
 
 export const options = {
   columns: [
@@ -49,7 +25,7 @@ export const options = {
 
 const { ListPage: RepositoriesListPage } = createCRUDComponents(options)
 
-export default compose(
-  requiresAuthentication,
-  withDataLoader({ dataKey: 'clusters', loaderFn: loadInfrastructure }),
-)(RepositoriesListPage)
+export default withDataLoader({
+  dataKey: ['clusters', 'repositories'],
+  loaderFn: [loadInfrastructure, loadRepositories],
+})(RepositoriesListPage)
