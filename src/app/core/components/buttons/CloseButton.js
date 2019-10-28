24311900ab4874565e1b943da11ae30fa07b7851
Xan Torres <xan.torres@gmail.com>
2019-10-28 18:22:41 +0700

    Change Close button color on hover

--- src/app/core/components/buttons/CloseButton.js
+++ src/app/core/components/buttons/CloseButton.js
@@ -2,25 +2,28 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import FontAwesomeIcon from 'core/components/FontAwesomeIcon'
 import { Link } from 'react-router-dom'
-import { makeStyles, createStyles } from '@material-ui/styles'
+import { makeStyles } from '@material-ui/styles'
 
-const useStyles = makeStyles(theme => createStyles({
-  root: {
+const useStyles = makeStyles(theme => ({
+  icon: {
     color: theme.palette.primary.main,
-  }
-})
-)
+    '&:hover': {
+      color: theme.palette.primary.dark,
+    },
+  },
+}))
+
 const CloseButton = props => {
   const classes = useStyles()
   if (props.to) {
     return (
       <Link to={props.to}>
-        <FontAwesomeIcon className={classes.root} solid size="2x" {...props}>times-circle</FontAwesomeIcon>
+        <FontAwesomeIcon className={classes.icon} solid size="2x" {...props}>times-circle</FontAwesomeIcon>
       </Link>
     )
   }
   return (
-    <FontAwesomeIcon className={classes.root} solid size="2x" {...props}>times-circle</FontAwesomeIcon>
+    <FontAwesomeIcon className={classes.icon} solid size="2x" {...props}>times-circle</FontAwesomeIcon>
   )
 }
 
