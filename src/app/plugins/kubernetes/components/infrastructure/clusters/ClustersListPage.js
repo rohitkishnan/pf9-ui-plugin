c245df1761ddbe2b1ea121cc7d8229c7e4ca99a5
Brennan Cheung <git@brennancheung.com>
2019-11-22 11:39:48 -0800

    Only allow cluster scaling from valid state (#503)

--- src/app/plugins/kubernetes/components/infrastructure/clusters/ClustersListPage.js
+++ src/app/plugins/kubernetes/components/infrastructure/clusters/ClustersListPage.js
@@ -178,8 +178,8 @@ const renderStats = (_, { usage }) => {
 const renderClusterDetailLink = (name, cluster) =>
   <SimpleLink src={`/ui/kubernetes/infrastructure/clusters/${cluster.uuid}`}>{name}</SimpleLink>
 
-const canScaleMasters = ([row]) => row.cloudProviderType === 'aws'
-const canScaleWorkers = ([row]) => row.cloudProviderType === 'aws'
+const canScaleMasters = ([row]) => row.taskStatus === 'success'
+const canScaleWorkers = ([row]) => row.taskStatus === 'success'
 const canUpgradeCluster = (selected) => false
 const canDeleteCluster = ([row]) => !(['creating', 'deleting'].includes(row.taskStatus))
 
