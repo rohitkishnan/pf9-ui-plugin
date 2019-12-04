739b6e626148f1f9d9d5801d699a4f11f275144a
Xan Torres <xan.torres@gmail.com>
2019-12-04 23:39:40 +0700

    Override role descriptions with custom text for admin and _member_ roles

--- src/app/plugins/kubernetes/components/userManagement/roles/actions.js
+++ src/app/plugins/kubernetes/components/userManagement/roles/actions.js
@@ -26,13 +26,7 @@ export const mngmRoleActions = createCRUDActions(mngmRolesCacheKey, {
 // Generally not a fan of this, but we dont have a lot of options.
 // Need the backend to wrap a service around keystone.getRoles to accept a platform / target
 // Once supported pass a platform though. Where platform is either 'kubernetes' | 'openstack'
-const hardcodedKubeRolesDescriptions = [
-  {
-    name: 'admin',
-    description: 'An Administrator has global access to all regions, tenants and all resources within your Platform9 environment, and access to perform all operations. As a Platform9 Managed Kubernetes (PMK) administrator you can invite more users to your PMK cloud environment, create or delete tenants, create or delete clusters, deploy workloads on clusters, assign RBAC policies to self-service users so they can access clusters,  etc.',
-  },
-  {
-    name: '_member_',
-    description: 'A self-service user has limited access to the Platform9 Managed Kubernetes (PMK) cloud environment. When an Administrator invites a self-service user to the PMK cloud environment, they can start by assigning the user to one or more tenants. The user will only be able to log into those tenants. Within the tenant, a self-service user will by default not have access to any clusters created in that tenant. The administrator will need to create specific RBAC policies to give the user access to one or more clusters within the tenant, before the users can perform any operations on the clusters.',
-  },
-]
+const hardcodedKubeRolesDescriptions = {
+  admin: 'An Administrator has global access to all regions, tenants and all resources within your Platform9 environment, and access to perform all operations. As a Platform9 Managed Kubernetes (PMK) administrator you can invite more users to your PMK cloud environment, create or delete tenants, create or delete clusters, deploy workloads on clusters, assign RBAC policies to self-service users so they can access clusters,  etc.',
+  _member_: 'A self-service user has limited access to the Platform9 Managed Kubernetes (PMK) cloud environment. When an Administrator invites a self-service user to the PMK cloud environment, they can start by assigning the user to one or more tenants. The user will only be able to log into those tenants. Within the tenant, a self-service user will by default not have access to any clusters created in that tenant. The administrator will need to create specific RBAC policies to give the user access to one or more clusters within the tenant, before the users can perform any operations on the clusters.',
+}
